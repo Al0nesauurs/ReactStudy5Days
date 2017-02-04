0bc7435a7d361896cc8772dee025fb89e9e4510a
KID <nakid001@gmail.com>
2017-02-04 13:12:39 +0700

    install react

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	devtool : 'eval',
+	entry :'./index.js',
+	output : {
+		publicPath: '/static/',
+		path: path.join(__dirname, 'static'),
+		filename: 'bundle.js'
+	},
+	
+	module: {
+		loaders:[
+		{
+			test: /\.(js|jsx)$/,
+			exclude: /node_modules/,
+			loaders: [
+			'babel-loader'
+			]
+		}
+	  ],
+	}
+};
